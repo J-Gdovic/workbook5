d18aace2ef5f3251cc128fd0380ea0fbb0c97062
J Gdovic <jgdovic3@gmail.com>
2023-11-07 14:26:23 -0500

    added ids to array objects

--- exercises/dynamicImages/scripts/dynamicImages.js
+++ exercises/dynamicImages/scripts/dynamicImages.js
@@ -1,32 +1,41 @@
 "use strict";
 
 let imageFiles = [
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
+  {id:"1", path: "https://placehold.co/300x300", description: "placeholder 1" },
+  {id:"2", path: "https://placehold.co/300x300", description: "placeholder 2" },
+  {id:"3", path: "https://placehold.co/300x300", description: "placeholder 3" },
+  {id:"4", path: "https://placehold.co/300x300", description: "placeholder 4" },
+  {id:"5", path: "https://placehold.co/300x300", description: "placeholder 5" },
+  {id:"6", path: "https://placehold.co/300x300", description: "placeholder 6" },
+  {id:"7", path: "https://placehold.co/300x300", description: "placeholder 7" },
+  {id:"8", path: "https://placehold.co/300x300", description: "placeholder 8" },
+  {id:"9", path: "https://placehold.co/300x300", description: "placeholder 9" },
+  {id:"10", path: "https://placehold.co/300x300", description: "placeholder 10" },
+  {id:"11", path: "https://placehold.co/300x300", description: "placeholder 11" },
+  {id:"12", path: "https://placehold.co/300x300", description: "placeholder 12" },
 ];
 // get elements into variables
 const imageList = document.querySelector("#imageList");
 const addImageButton = document.querySelector("#addImageButton");
 const clearImageButton = document.querySelector("#clearImageButton");
 const imagesDiv = document.querySelector("#imagesDiv");
-const option = document.querySelectorAll("option");
+
 
 //console.log(selectImageList,addImageButton,clearImageButton,imagesDiv);
 // functions
 
 function loadImageList() {
     for (const imageFile of imageFiles) {
-        console.log(imageFile);
+        // baby step console.log(imageFile);
+        const option = document.createElement("option");
+        option.value = imageFile.path;
+        option.innerText = imageFile.description;
+        imageList.appendChild(option);
     }
 }
+
+function addImageButtonOnClick() {
+    
+}
+
 loadImageList();
