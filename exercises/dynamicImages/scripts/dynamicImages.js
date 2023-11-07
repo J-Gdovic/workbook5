7263cf48045e7f188d326f1ae483e4a72ef2e7ec
J Gdovic <jgdovic3@gmail.com>
2023-11-07 15:35:49 -0500

    got place holder pictures to show onclick

--- exercises/dynamicImages/scripts/dynamicImages.js
+++ exercises/dynamicImages/scripts/dynamicImages.js
@@ -1,18 +1,18 @@
 "use strict";
 
 let imageFiles = [
-  {id:"1", path: "https://placehold.co/300x300", description: "placeholder 1" },
-  {id:"2", path: "https://placehold.co/300x300", description: "placeholder 2" },
-  {id:"3", path: "https://placehold.co/300x300", description: "placeholder 3" },
-  {id:"4", path: "https://placehold.co/300x300", description: "placeholder 4" },
-  {id:"5", path: "https://placehold.co/300x300", description: "placeholder 5" },
-  {id:"6", path: "https://placehold.co/300x300", description: "placeholder 6" },
-  {id:"7", path: "https://placehold.co/300x300", description: "placeholder 7" },
-  {id:"8", path: "https://placehold.co/300x300", description: "placeholder 8" },
-  {id:"9", path: "https://placehold.co/300x300", description: "placeholder 9" },
-  {id:"10", path: "https://placehold.co/300x300", description: "placeholder 10" },
-  {id:"11", path: "https://placehold.co/300x300", description: "placeholder 11" },
-  {id:"12", path: "https://placehold.co/300x300", description: "placeholder 12" },
+  { id: "1", path: "https://placehold.co/300x300", description: "placeholder 1" },
+  { id: "2", path: "https://placehold.co/300x300", description: "placeholder 2" },
+  { id: "3", path: "https://placehold.co/300x300", description: "placeholder 3" },
+  { id: "4", path: "https://placehold.co/300x300", description: "placeholder 4" },
+  { id: "5", path: "https://placehold.co/300x300", description: "placeholder 5" },
+  { id: "6", path: "https://placehold.co/300x300", description: "placeholder 6" },
+  { id: "7", path: "https://placehold.co/300x300", description: "placeholder 7" },
+  { id: "8", path: "https://placehold.co/300x300", description: "placeholder 8" },
+  { id: "9", path: "https://placehold.co/300x300", description: "placeholder 9" },
+  { id: "10", path: "https://placehold.co/300x300", description: "placeholder 10" },
+  { id: "11", path: "https://placehold.co/300x300", description: "placeholder 11" },
+  { id: "12", path: "https://placehold.co/300x300", description: "placeholder 12" },
 ];
 // get elements into variables
 const imageList = document.querySelector("#imageList");
@@ -20,24 +20,37 @@ const addImageButton = document.querySelector("#addImageButton");
 const clearImageButton = document.querySelector("#clearImageButton");
 const imagesDiv = document.querySelector("#imagesDiv");
 
-
 //console.log(selectImageList,addImageButton,clearImageButton,imagesDiv);
 // functions
 
 function loadImageList() {
-    for (const imageFile of imageFiles) {
-        // baby step console.log(imageFile);
-        const option = document.createElement("option");
-        option.value = imageFile.id;
-        option.innerText = imageFile.description;
-        imageList.appendChild(option);
-    }
+  for (const imageFile of imageFiles) {
+    // baby step console.log(imageFile);
+    const option = document.createElement("option");
+    option.value = imageFile.id;
+    option.innerText = imageFile.description;
+    imageList.appendChild(option);
+  }
 }
 
 function addImage() {
   // baby step to make sure function is working in the console  console.log("working");
-  
-
+  const imageFileId = imageList.value;
+  // baby step to make sure imageList.value is defined - console.log(imageFileId);
+  for (const imageFile of imageFiles) {
+    // baby step to see the imageFile loop through console.log(imageFile);
+    if (imageFile.id == imageFileId) {
+      // baby step to show imageFile.id in the console - console.log(imageFile);
+      //create image element
+      const image = document.createElement("img");
+      // set src attribute on image to imageFiles path
+      image.src = imageFile.path;
+      //set the alt on the image to the imageFiles description
+      image.alt = imageFile.description;
+      //add image to div
+      imagesDiv.appendChild(image);
+    }
+  }
 }
 
 loadImageList();
