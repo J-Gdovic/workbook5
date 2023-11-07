f0e27d4cb7eccdc6ecd9e88d3c9febd55e4b3f3f
J Gdovic <jgdovic3@gmail.com>
2023-11-07 11:06:14 -0500

    image round

--- exercises/imagesDemo/scripts/images.js
+++ exercises/imagesDemo/scripts/images.js
@@ -1,15 +1,18 @@
 "use strict";
 // get elements into variables
 const decorateButton = document.querySelector("#decorateButton");
-const image = document.querySelectorAll("img");
+const images = document.querySelectorAll("img");
 const paragraphs = document.querySelectorAll("p");
 
 //functions
 function decorate() {
     for (const paragraph of paragraphs) {
-    paragraph.style.backgroundColor = "yellow";  
+    paragraph.style.border = "1rem dashed red"; 
+    paragraph.style.backgroundColor = "green"; 
     }
-  
+    for (const image of images) {
+        image.className = "roundedImg";
+    }
 }
 
 
