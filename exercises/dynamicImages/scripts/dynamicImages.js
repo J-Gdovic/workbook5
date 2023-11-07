74229f638c1920d3fe9e8a74804fcc5ac4de1e79
J Gdovic <jgdovic3@gmail.com>
2023-11-07 13:49:15 -0500

    start of loop function

--- exercises/dynamicImages/scripts/dynamicImages.js
+++ exercises/dynamicImages/scripts/dynamicImages.js
@@ -1,1 +1,32 @@
 "use strict";
+
+let imageFiles = [
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder" },
+];
+// get elements into variables
+const imageList = document.querySelector("#imageList");
+const addImageButton = document.querySelector("#addImageButton");
+const clearImageButton = document.querySelector("#clearImageButton");
+const imagesDiv = document.querySelector("#imagesDiv");
+const option = document.querySelectorAll("option");
+
+//console.log(selectImageList,addImageButton,clearImageButton,imagesDiv);
+// functions
+
+function loadImageList() {
+    for (const imageFile of imageFiles) {
+        console.log(imageFile);
+    }
+}
+loadImageList();
