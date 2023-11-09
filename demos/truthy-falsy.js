5a9cd866b2a2f090ba339a6bfc0166b2c573b8c1
J Gdovic <jgdovic3@gmail.com>
2023-11-09 11:29:05 -0500

    truthy and falsy demo

---
+++ demos/truthy-falsy.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let value;
+value = "";
+value= undefined;
+value = null;
+
+if (value == "" || value == undefined || value == null) {
+  console.log(false);
+}
+
+if(!value){
+ console.log(false);
+}
+
