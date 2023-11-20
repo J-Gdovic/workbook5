acdfafdaf01d3c892e561274b5804dab6b44f342
J Gdovic <jgdovic3@gmail.com>
2023-11-20 11:30:50 -0500

    push and find many

---
+++ review/array-push.js
@@ -0,0 +1,24 @@
+let numbers = [];
+console.log(numbers);
+//.push adds values to an array
+numbers.push(1);
+console.log(numbers);
+numbers.push(2);
+console.log(numbers);
+numbers.push(3);
+console.log(numbers);
+numbers.push(4);
+console.log(numbers);
+
+
+let students = [];
+console.log(students);
+
+students.push("Salin");
+console.log(students);
+students.push("Javier");
+console.log(students);
+students.push("Teddy");
+console.log(students);
+students.push("Ian");
+console.log(students);
