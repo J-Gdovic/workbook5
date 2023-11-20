7a38d03994c40d1eb07c7b184d4f53c1fac364fb
J Gdovic <jgdovic3@gmail.com>
2023-11-20 13:15:46 -0500

    trim string

--- review/trim-string.js
+++ review/trim-string.js
@@ -2,5 +2,5 @@ let value = "  Would Craig give us the test already?  ";
 console.log(value.trim());
 
 
-//trimStart -- start of  string 
-//trimEnd -- end of string
+//.trimStart() -- start of  string 
+//.trimEnd() -- end of string
