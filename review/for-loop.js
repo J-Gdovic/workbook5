9306d973ea8b7afe68abb305367173f9d223f46c
J Gdovic <jgdovic3@gmail.com>
2023-11-20 10:50:21 -0500

    for loops and if comparisons review

---
+++ review/for-loop.js
@@ -0,0 +1,20 @@
+let colors = ["red","green","blue","ppurple","yellow","orange"];
+
+
+//forof loop
+// for (const color of colors) {
+//     console.log(color);
+// }
+// console.log(colors[2]);
+
+
+//for loop
+// for (let index = 0; index < array.length; index++) { 
+//     const element = array[index];
+    
+// }
+
+for (let index = 0; index < colors.length; index++) {
+    const color = colors[index];
+    console.log(color);
+}
---
+++ review/if-comparison.js
@@ -0,0 +1,12 @@
+let message = "fail"; // assignment
+
+if (message == "fail") {
+  //comparisions equal
+  console.log("failed");
+}
+
+message = "pass";
+if (message != "fail") {
+  //comparison not equal
+  console.log("succeeded");
+}
