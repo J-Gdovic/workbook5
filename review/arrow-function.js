0f254954a92f102d482a1b70d801a01649c9a0f2
J Gdovic <jgdovic3@gmail.com>
2023-11-20 13:10:02 -0500

    arrow function example

---
+++ review/arrow-function.js
@@ -0,0 +1,12 @@
+//normal function
+function bob(a) {
+    return a + 100
+}
+let result = bob(10);
+console.log(result);
+
+
+// arrow function
+const bob2 = (a) => a + 100;
+let result2 = bob(10);
+console.log(result2);
