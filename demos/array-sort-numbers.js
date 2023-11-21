5056f913ff58acb665970734f3557a9afa7afca4
J Gdovic <jgdovic3@gmail.com>
2023-11-21 13:51:23 -0500

    array sort numbers ascending

---
+++ demos/array-sort-numbers.js
@@ -0,0 +1,17 @@
+let numbers = [100, 20, 55, 67, 11, 2, 88];
+
+//long version
+// function compare(a,b) {
+//     if (a < b) return -1; // right order
+// else if (a == b) return 0;
+// else return 1; // swap, wrong order
+// }
+// numbers.sort(compare);
+// console.log(numbers);
+
+//shorter version
+function compare(a,b) {
+return a-b;
+}
+numbers.sort(compare);
+console.log(numbers);
